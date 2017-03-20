ff452405e89aa31b947c079c33e6b0621c60c14e
arjunkhode <arjunkhode@gmail.com>
2017-03-20 16:53:24 -0700

    Added CSS and looped over videolistitem

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { render } from 'react-dom';
 
-const SearchBar = () => {
-	return(<input/>);
+class SearchBar extends React.Component{
+	constructor(props){
+		super();
+		this.state = {term:''};
+	}
+	render(){
+		return(
+			<input onChange = {event => this.setState({term:event.target.value})}/>);
+	}
 };
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import {render} from 'react-dom';
 import SearchBar from './components/SearchBar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyAb_9LBQD0qQWbahjXYiSW0BdvYV258EfQ';
+
 
 class App extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {videos:[]};
+
+		YTSearch({key: API_KEY, term: 'surfboards'},
+		(videos) => {this.setState({videos})});
+	}
 	render(){
-		return(<SearchBar/>);
+		return(
+		<div>
+			<SearchBar/>
+			<VideoList videos={this.state.videos}/>
+		</div>
+		);
 	};
 }
 
